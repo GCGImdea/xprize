3f233db69aecd77c17bf001a04e318c1969f9b56
antonio_fernandez <antonio.fernandez@imdea.org>
2020-12-18 20:42:25 +0100

    forecast

--- code/forecast-strawman.R
+++ code/forecast-strawman.R
@@ -1,81 +1,66 @@
 library(dplyr)
 library(stringr)
+library(tidyverse)
+library(lubridate)
 
 ox_country_path <- "../data/oxford/country/" # Oxford data
-confirmed_country_path <- "../data/estimates-confirmed/" # Confirmed data from ECDC
-ccfr_country_path <- "../data/estimates-ccfr-based/" # CCFR estimates
-hospital_country_path <- "../data/estimates-confirmed-hospital/" # Hospital data from ECDC
-W_country_path <- "../data/estimates-W/smooth/" # Estimates from the CoronaSurveys poll
-umd_api_country_path <- "../data/estimates-umd-symptom-survey/" # Data from UMD Symptom Survey via API
-umd_challenge_country_path <- "../data/estimates-umd-unbatched/PlotData/" # Data from UMD Symptom Survey via Challenge
+ox_region_path <- "../data/oxford/region/"
 
-output_path <- "../data/all_giant_df2/"
+output_file <- "../work/whole-forecast.csv"
 
-start_date <- "2020-01-01"
-
-country_dataset <- function(code,
-                            df_giant,
-                            prefix = "conf_",
-                            country_path = confirmed_country_path,
-                            file_postfix = "-estimate.csv")
-  {
-  date_col <- paste0(prefix, "date")
-  file_name <- paste0(country_path, code, file_postfix)
-  if (file.exists(file_name)){
-    cat("File", file_name, "exists!\n")
-    df_aux <- read.csv(file_name)
-    names(df_aux) <- tolower(names(df_aux))
-    names(df_aux) <- paste0(prefix, names(df_aux))
-    #df_aux$date <- as.Date(date_aux)
-    df_aux$date <- as.Date(df_aux[,date_col])
-    df_giant <- df_giant %>% full_join(df_aux, by = "date")
-  }
-  return(df_giant)
-  }
+start_date <- ymd("2020-01-01")
+end_date <- ymd("2021-01-31")
 
 
-load_and_combine_country <- function(code, nsum = FALSE) {
+process_country <- function(code, file_path) {
   
   cat("\n working on ", code, "\n")
   
   # Initialize with Oxford data
-  df_giant <- read.csv(paste0(ox_country_path, code, "-estimate.csv"))
-  names(df_giant) <- tolower(names(df_giant))
-  df_giant$date <- as.Date(df_giant$date)
+  df <- read.csv(paste0(file_path, code, "-estimate.csv"))
+  df <- df %>%
+    select(CountryName, RegionName, Date, PredictedDailyNewCases=cases)
+  df$Date <- as.Date(df$Date)
+  df$RegionName[is.na(df$RegionName)] <- ""
   
-  df_giant <- country_dataset(code, df_giant, prefix = "conf_", country_path = confirmed_country_path,
-                  file_postfix = "-estimate.csv")
+  first_non_NA <- min(which(!is.na(df$PredictedDailyNewCases)))
+  df$PredictedDailyNewCases[1:first_non_NA] <- 0
+  df <- df[!is.na(df$PredictedDailyNewCases),]
   
-  df_giant <- country_dataset(code, df_giant, prefix = "ccfr_", country_path = ccfr_country_path,
-                  file_postfix = "-estimate.csv")
+  df <- df %>% 
+    complete(Date = seq.Date(start_date, end_date, by="day")) %>% 
+    fill(CountryName, RegionName, PredictedDailyNewCases)
   
-  df_giant <- country_dataset(code, df_giant, prefix = "hosp_", country_path = hospital_country_path,
-                  file_postfix = "-hospital-icu.csv")
-  
-  # df_giant <- country_dataset(code, df_giant, prefix = "W_", country_path = W_country_path,
-  #                 file_postfix = "-estimate-past-smooth.csv")
-
-  df_giant <- country_dataset(code, df_giant, prefix = "W_", country_path = W_country_path,
-                  file_postfix = "-estimate.csv")
-  
-  df_giant <- country_dataset(code, df_giant, prefix = "umdapi_", country_path = umd_api_country_path,
-                  file_postfix = "-estimate.csv")
-  
-  df_giant <- country_dataset(code, df_giant, prefix = "umdchl_", country_path = umd_challenge_country_path,
-                  file_postfix = "_UMD_country_nobatch_past_smooth.csv")
-  
-  df_giant <- df_giant[df_giant$date >= start_date,]
-  df_giant <- df_giant[order(df_giant$date),]
-  out_path <- paste0(output_path, code, "-alldf.csv")
-  write.csv(df_giant, out_path, row.names = FALSE)
-  cat("[saved data]")
+  return(df)
 }
 
+
+df <- data.frame(CountryName=character(),
+                 RegionName=character(),
+                 Date=as.Date(character()),
+                 PredictedDailyNewCases=double(),
+                 stringsAsFactors=FALSE) 
+  
+#  data.frame(column.names = c("CountryName", "RegionName", "Date", "PredictedDailyNewCases"))
 #save data for countries of interest
 
 interest <- list.files(ox_country_path, pattern="*.csv", full.names=FALSE)
 interest <- substring(interest, 1, 2)
 
-dd <- sapply(interest, load_and_combine_country, nsum = F)
+for (c in interest) {
+  df <- bind_rows(df, process_country(c, file_path=ox_country_path))
+}
+
+interest <- list.files(ox_region_path, pattern="*.csv", full.names=FALSE)
+interest <- word(interest,1,sep = "-")
+
+for (c in interest) {
+  df <- bind_rows(df, process_country(c, file_path=ox_region_path))
+}
+
+df <- df[order(df$CountryName,df$RegionName,df$Date),]
+write.csv(df,output_file, row.names = F)
+
+#dd <- sapply(interest, load_and_combine_country, nsum = F)
 
 
