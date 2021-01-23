7f5eae77ce92487442288da534ed031fe4c325cc
Harold A. Hdez <34509768+hhroig@users.noreply.github.com>
2021-01-23 19:56:09 +0100

    add Rt in some countries/regions

--- code/script-get-oxford-data.R
+++ code/script-get-oxford-data.R
@@ -5,6 +5,62 @@ library(httr)
 library(jsonlite)
 library(stringr)
 library(data.table)
+library(R0) # reproductive number
+
+# Function to compute R:
+errors_Rt_country <- c()
+errors_Rt_region <- c()
+var_to_Rt = "avgcases7days"
+# var_to_Rt = "cases"
+
+do_Rt <- function(df, var_to_Rt = "cases", gamma_mean = 5.20, gamma_sd = 1.72) {
+  
+  # generation and serial interval distributions, taken from:
+  # https://www.eurosurveillance.org/content/10.2807/1560-7917.ES.2020.25.17.2000257
+  # Dataset	        Scenario	Interval	Estimate (95% credible interval) (days)
+  #                               Mean	                  SD
+  # Singapore	      Baseline	GI	5.20 (3.78 - 6.78)	  1.72 (0.91 - 3.93)
+  #                           SI	5.21 (???3.35 - 13.94)	4.32 (4.06 - 5.58)
+  # Tianjin (China)	Baseline	GI	3.95 (3.01 - 4.91)	  1.51 (0.74 - 2.97)
+  #                           SI	3.95 (???4.47 - 12.51)	4.24 (4.03 - 4.95)
+  
+  cat("-> computing Rt: using signal", var_to_Rt, "\n")
+  
+  # select the target to compute Rt
+  temp_for_R0 <- df[, var_to_Rt]
+  
+  # assign dates to rows
+  names(temp_for_R0) <- df$Date
+  
+  # first non-zero elements:
+  indx <- which(temp_for_R0 != 0)
+  temp_for_R0 <- temp_for_R0[indx[1L]:indx[length(indx)]]
+  
+  # generation time function
+  GT_covid <- generation.time("gamma", c(gamma_mean,gamma_sd))
+  
+  res_R <- estimate.R(temp_for_R0, 
+                      GT = GT_covid, 
+                      t = as.Date(names(temp_for_R0)),
+                      begin = (head(temp_for_R0, 1) %>%  names()), 
+                      end = (tail(temp_for_R0, 1) %>%  names()),
+                      pop.size = max(df$population),
+                      # methods = c("EG","ML","SB","TD"))
+                      methods = c("TD"))
+  
+  # save the results to a single data frame:
+  Rt_CI <- res_R[["estimates"]][["TD"]][["conf.int"]] %>% 
+    transmute(Rt = res_R[["estimates"]][["TD"]][["R"]],
+              Rt_lower = lower,
+              Rt_upper = upper,
+              Date = as.Date(rownames(res_R[["estimates"]][["TD"]][["conf.int"]])))
+  colnames(Rt_CI)[1:3] <- paste0(colnames(Rt_CI)[1:3], "_", var_to_Rt)
+  
+  df_out <- left_join(df, Rt_CI, by = "Date")
+  
+  return(df_out)
+}
+
 
 DATA_URL = "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv"
 country_file <- "../data/common-data/oxford-countries.csv"
@@ -55,6 +111,22 @@ for (country in all_countries) {
   geoid <- c_data[c_data$CountryName == country,"geo_id"]
   df$population <- c_data[c_data$CountryName == country,"population"]
   df$iso2 <- geoid
+   
+    # Compute the Rt:
+    tryCatch(
+      expr = {
+        df <- do_Rt(df, var_to_Rt = var_to_Rt)
+      },
+      error = function(e){ 
+        
+        cat("Error while computing Rt for", 
+            unique(country), "\n")
+        errors_Rt_country <- c(errors_Rt_country, country)
+        
+      }
+    )
+  
+  
   write.csv(df, paste0(output_path, "country/", geoid, "-estimate.csv"),
             row.names = FALSE)
 }
@@ -89,6 +161,21 @@ for (region in all_regions) {
   region_code <- df$RegionCode[1]
   df$population <- region_list[region_list$RegionName == region,"Population"]
   df$iso2 <- region_code
+  
+  # Compute the Rt:
+  tryCatch(
+    expr = {
+      df <- do_Rt(df, var_to_Rt = var_to_Rt)
+    },
+    error = function(e){ 
+      
+      cat("Error while computing Rt for", 
+          unique(df$CountryName),  region, "\n")
+      errors_Rt_region <- c(errors_Rt_region, region)
+      
+    }
+  )
+  
   write.csv(df, paste0(output_path, "region/", region_code, "-estimate.csv"),
             row.names = FALSE)
 }
