f3c0c240acdddd5d629e1e2839aabbebcc52a5f2
sigmoidal <stathis@npcglib.org>
2021-01-28 23:00:27 +0200

    Update prescribe.R

--- work-phase2/prescribe5/prescribe.R
+++ work-phase2/prescribe5/prescribe.R
@@ -1,7 +1,3 @@
-list.of.packages <- c("tidyverse")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages, repos="http://cran.r-project.org")
-
 # load library
 library(tidyverse)
 # library(readxl)
