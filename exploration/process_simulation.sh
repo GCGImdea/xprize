63f7514eb51a34da7375d87b299a0dbedc6aa3a4
antonio_fernandez <antonio.fernandez@imdea.org>
2021-01-30 10:42:18 +0100

    exhaustive

--- exploration/process_simulation.sh
+++ exploration/process_simulation.sh
@@ -1,7 +1,34 @@
 # creates a file per colection of intervention vectors
 
-for fullfile in ./predictor-dfs-with-HX/*.csv
+for fullfile in ./ips-vectors-full/*.csv
 do
   filename=$(basename -- "$fullfile")
-  Rscript process_simulation.R $fullfile predictions-with-Rt/${filename}
+
+  simfile=./predictions-raw/predicted-${filename}
+  if [[ ! -f ${simfile} ]]
+  then
+    echo "${simfile} does not exist on your filesystem."
+  fi
+
+  ipsfile=./ips-vectors/${filename}
+  if [[ ! -f ${ipsfile} ]]
+  then
+    echo "${ipsfile} does not exist on your filesystem. Generating it..."
+    head -1 ${fullfile} > kk1.$$
+    grep "2021-01-01" ${fullfile} | sed 's/2021/2020/' > kk2.$$
+    cat kk1.$$ kk2.$$ > ${ipsfile}
+    rm kk1.$$ kk2.$$
+  fi
+done
+
+
+for fullfile in ./predictions-raw/*.csv
+do
+  filename=$(basename -- "$fullfile")
+  simfile=./predictions-with-Rt/${filename}
+  if [[ ! -f ${simfile} ]]
+  then
+    echo "${simfile} does not exist on your filesystem. Running process..."
+    Rscript process_simulation.R $fullfile ${simfile}
+  fi
 done
