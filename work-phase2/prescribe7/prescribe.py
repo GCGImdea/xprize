e5bd4dc28e74ef308ec0cb6da20877c81d2dcd55
Rafael Garcia Leiva <rafael.garcia@imdea.org>
2021-02-03 20:27:04 +0100

    Support for timing
    Added support for timing

--- work-phase2/prescribe7/prescribe.py
+++ work-phase2/prescribe7/prescribe.py
@@ -7,6 +7,15 @@ import pandas as pd
 
 from datetime import timedelta
 
+import sys
+import os
+import subprocess
+import re
+import time
+
+sys.path.append(os.path.expanduser("~/work/logger"))
+import utils
+
 IP_COLS = ['C1_School closing',
            'C2_Workplace closing',
            'C3_Cancel public events',
@@ -255,6 +264,44 @@ if __name__ == '__main__':
                         required=True,
                         help="The path to an intervention plan .csv file")
     args = parser.parse_args()
-    print(f"Generating prescriptions from {args.start_date} to {args.end_date}...")
-    prescribe(args.start_date, args.end_date, args.prior_ips_file, args.cost_file, args.output_file)
-    print("Done!")
+
+    start = time.time()
+
+    rScriptFile, ext = os.path.splitext(sys.argv[0])
+    rScriptFile += ".R"
+
+    log_name = "default"
+    matches = re.findall(r'/(prescribe\d+)', os.path.dirname(os.path.realpath(__file__)))
+    if len(matches) > 0:
+        log_name = matches[0]
+
+    logger = utils.named_log(str(log_name))
+
+    rScriptFile, ext = os.path.splitext(sys.argv[0])
+    rScriptFile += ".R"
+
+    try:
+        r_cmd = [
+            "Rscript",
+            "--vanilla",
+            rScriptFile,
+            args.start_date,
+            args.end_date,
+            os.path.expanduser(args.prev_file),
+            os.path.expanduser(args.cost_file),
+            os.path.expanduser(args.output_file),
+            os.path.dirname(os.path.realpath(__file__))
+        ]
+
+        logger.info("R command: " + ' '.join(r_cmd))
+
+        subprocess.call(r_cmd)
+    except OSError as error:
+        logger.info(error)
+    except:
+        logger.info("Unexpected error: %s", sys.exc_info()[0])
+        raise
+    else:
+        logger.info("Successfully executed %s", os.path.realpath(__file__))
+
+    logger.info("Duration: %s seconds", utils.secondsToStr(time.time() - start))
